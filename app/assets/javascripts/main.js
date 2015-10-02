4e3b732327ddd6f9249a63a1c6f801fe002b9fdf
DanielAlk <daniodfad@yahoo.com.ar>
2015-10-02 06:18:27 -0300

    change bunker parallax height

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,27 @@
-var controller = new ScrollMagic.Controller();
-$(function () { 
+var BunkerAnimation = {}
+BunkerAnimation.controller = new ScrollMagic.Controller();
+BunkerAnimation.calculateTop = function(val) {
+	return BunkerAnimation.duration * val;
+};
+BunkerAnimation.setScene = function(duration) {
+	BunkerAnimation.duration = duration;
+	var controller = BunkerAnimation.controller;
+	var calculateTop = BunkerAnimation.calculateTop;
 	var tween = new TimelineMax ()
 	.add([
-		TweenMax.to("#parallaxBunker .bunker-layer-0", 1, {top: 475, ease: Linear.easeNone}),
-		TweenMax.to("#parallaxBunker .bunker-layer-1", 1, {top: 650, ease: Linear.easeNone}),
-		TweenMax.to("#parallaxBunker .bunker-layer-2", 1, {top: 500, ease: Linear.easeNone}),
-		TweenMax.to("#parallaxBunker .bunker-layer-3", 1, {top: 450, ease: Linear.easeNone}),
-		TweenMax.to("#parallaxBunker .bunker-layer-4", 1, {top: 350, ease: Linear.easeNone}),
-		TweenMax.to("#parallaxBunker .bunker-layer-5", 1, {top: 300, ease: Linear.easeNone}),
-		TweenMax.to("#parallaxBunker .bunker-layer-6", 1, {top: 0, ease: Linear.easeNone})
+		TweenMax.to("#parallaxBunker .bunker-layer-0", 1, {top: calculateTop(0.68), ease: Linear.easeNone}),
+		TweenMax.to("#parallaxBunker .bunker-layer-1", 1, {top: calculateTop(0.93), ease: Linear.easeNone}),
+		TweenMax.to("#parallaxBunker .bunker-layer-2", 1, {top: calculateTop(0.715), ease: Linear.easeNone}),
+		TweenMax.to("#parallaxBunker .bunker-layer-3", 1, {top: calculateTop(0.64), ease: Linear.easeNone}),
+		TweenMax.to("#parallaxBunker .bunker-layer-4", 1, {top: calculateTop(0.5), ease: Linear.easeNone}),
+		TweenMax.to("#parallaxBunker .bunker-layer-5", 1, {top: calculateTop(0.43), ease: Linear.easeNone}),
+		TweenMax.to("#parallaxBunker .bunker-layer-6", 1, {top: calculateTop(0), ease: Linear.easeNone})
 	]);
-
-	var scene = new ScrollMagic.Scene({duration: 700})
+	BunkerAnimation.scene = new ScrollMagic.Scene({duration: duration})
 	.setTween(tween)
 	.addTo(controller);
+};
+
+$(function() { 
+	BunkerAnimation.setScene($('#parallaxBunker').outerHeight());
 });
