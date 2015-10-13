64e2c1cafa17c21ca965675c0eda537a840d3b5d
DanielAlk <daniodfad@yahoo.com.ar>
2015-10-13 09:58:41 -0300

    attempt minifier bug

--- app/assets/javascripts/main.php
+++ app/assets/javascripts/main.php
@@ -1,11 +1,11 @@
 <?php
 #INCLUDE ALL JS FILES BELOW
-include 'jquery-1.11.0.min.js'; echo "\r\n";
-include 'jquery-ui-effects.js'; echo "\r\n";
-include 'TweenMax.min.js'; echo "\r\n";
-include 'ScrollMagic.min.js'; echo "\r\n";
-include 'animation.gsap.min.js'; echo "\r\n";
-include 'bootstrap.min.js'; echo "\r\n";
-include 'bunker.js'; echo "\r\n";
-include 'main.js'; echo "\r\n";
+include 'jquery-1.11.0.min.js';
+include 'jquery-ui-effects.js';
+include 'TweenMax.min.js';
+include 'ScrollMagic.min.js';
+include 'animation.gsap.min.js';
+include 'bootstrap.min.js';
+include 'bunker.js';
+include 'main.js';
 ?>
