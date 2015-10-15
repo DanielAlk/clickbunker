9c7a9e459c793b97ba82e2320934cb1081194132
DanielAlk <daniodfad@yahoo.com.ar>
2015-10-15 14:15:43 -0300

    separate bunker content

--- app/views/home/_bunker.php
+++ app/views/home/_bunker.php
@@ -8,9 +8,7 @@
 			<div class="bunker-layer-4"></div>
 			<div class="bunker-layer-5">
 				<div class="container">
-					<h1>Atrae y convierte clicks en clientes con <b class="block">Inbound Marketing</b></h1>
-					<p>Hacé crecer tu negocio con Inbound Marketing. Clickbunker te ayudará a convertir tu plan de Marketing tradicional en una fábrica dinámica de Leads.</p>
-					<div><a class="btn btn-primary scroll-to" href="#inbound" data-delay="1200">Descubre cómo</a></div>
+					<?php include 'home/_bunker_content.php'; ?>
 				</div>
 			</div>
 			<div class="bunker-layer-6"></div>
@@ -21,9 +19,7 @@
 		<div class="bunker">
 			<div class="bunker-background">
 				<div class="container">
-					<h1>Atrae y convierte clicks en clientes con <b class="block">Inbound Marketing</b></h1>
-					<p>Hacé crecer tu negocio con Inbound Marketing. Clickbunker te ayudará a convertir tu plan de Marketing tradicional en una fábrica dinámica de Leads.</p>
-					<div><a class="btn btn-primary scroll-to" href="#inbound" data-delay="1200">Descubre cómo</a></div>
+					<?php include 'home/_bunker_content.php'; ?>
 				</div>
 			</div>
 		</div>
---
+++ app/views/home/_bunker_content.php
@@ -0,0 +1,3 @@
+<h1>Atrae y convierte clicks en clientes con <b class="block">Inbound Marketing</b></h1>
+<p>Hacé crecer tu negocio con Inbound Marketing. Clickbunker te ayudará a convertir tu plan de Marketing tradicional en una fábrica dinámica de Leads.</p>
+<div><a class="btn btn-primary scroll-to" href="#inbound" data-delay="1200">Descubre cómo</a></div>
